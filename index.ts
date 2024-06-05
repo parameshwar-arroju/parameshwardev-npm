1fb81a9d46424ecf25e3569bd2659f18b0a3c0dc
parameshwar-arroju <parameshwarshiva119@gmail.com>
2024-06-05 17:53:47 +0530

    Implemented Ascii Art

--- index.ts
+++ index.ts
@@ -2,21 +2,23 @@ import chalk from "chalk";
 import boxen from "boxen";
 
 // ASCII Logo
-const asciiLogo = `
-⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿
-⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢺⣿
-⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠆⠜⣿
-⣿⣿⣿⣿⠿⠿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿
-⣿⣿⡏⠁⠀⠀⠀⠀⠀⣀⣠⣤⣤⣶⣶⣶⣶⣶⣦⣤⡄⠀⠀⠀⠀⢀⣴⣿
-⣿⣿⣷⣄⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡧⠇⢀⣤⣶
-⣿⣿⣿⣿⣿⣿⣾⣮⣭⣿⡻⣽⣒⠀⣤⣜⣭⠐⢐⣒⠢⢰
-⣿⣿⣿⣿⣿⣿⣿⣏⣿⣿⣿⣿⣿⣿⡟⣾⣿⠂⢈⢿⣷⣞
-⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣷⣶⣾⡿⠿⣿⠗⠈⢻⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠻⠋⠉⠑⠀⠀⢘⢻
-⣿⣿⣿⣿⣿⣿⣿⡿⠟⢹⣿⣿⡇⢀⣶⣶⠴⠶⠀⠀⢽
-⣿⣿⣿⣿⣿⣿⡿⠀⠀⢸⣿⣿⠀⠀⠣⠀⠀⠀⠀⠀⡟⢿⣿
-⣿⣿⣿⡿⠟⠋⠀⠀⠀⠀⠹⣿⣧⣀⠀⠀⠀⠀⡀⣴⠁⢘⡙
-⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⠗⠂⠄⠀⣴⡟⠂⠈⠉    
+const asciiLogo = `          
+          ██████╗ ██╗  ██╗
+          ╚════██╗╚██╗██╔╝
+           █████╔╝ ╚███╔╝
+          ██╔═══╝  ██╔██╗
+          ███████╗██╔╝ ██╗
+          ╚══════╝╚═╝  ╚═╝
+          
+                  ██╗
+                  ╚═╝
+
+  ██████╗ ███████╗██╗   ██╗███████╗
+  ██╔══██╗██╔════╝██║   ██║██╔════╝
+  ██║  ██║█████╗  ██║   ██║███████╗
+  ██║  ██║██╔══╝  ╚██╗ ██╔╝╚════██║
+  ██████╔╝███████╗ ╚████╔╝ ███████║
+  ╚═════╝ ╚══════╝  ╚═══╝  ╚══════╝
 `;
 
 const details = {
@@ -24,78 +26,127 @@ const details = {
     occupation: chalk.magenta("A Full Stack developer"),
     website: chalk.blue(`https://parameshwar.dev`),
     github: chalk.green(`https://github.com/parameshwar-arroju`),
-    twitter: chalk.cyan(`https://twitter.com/`),
-    email: chalk.red(`   parameshwararroju1@gmail.com`),
+    email: chalk.cyan(`parameshwararroju1@gmail.com`),
     languages: {
-        JavaScript: { icon: "🟨", color: chalk.yellow },
-        Typescript: { icon: "🟦", color: chalk.blueBright },
-        Python: { icon: "🐍", color: chalk.green },
-        JAVA: { icon: "🔵", color: chalk.blue },
+        C: { icon: "🟨", color: chalk.yellow },
+        "C++": { icon: "🟨", color: chalk.yellow },
+        Java: { icon: "🟨", color: chalk.yellow },
+        Python: { icon: "🟨", color: chalk.yellow },
+    },
+    web: {
+        HTML: { icon: "🔵", color: chalk.blue },
+        CSS: { icon: "🔵", color: chalk.blue },
+        JavaScript: { icon: "🔵", color: chalk.blue },
+        Typescript: { icon: "🔵", color: chalk.blue },
+    },
+    libraries: {
+        ReactJs: { icon: "🟩", color: chalk.green },
+        TailwindCSS: { icon: "🟩", color: chalk.green },
     },
     frameworks: {
-        NextJs: { icon: "🌐 ", color: chalk.yellow },
-        ExpressJs: { icon: "🚂", color: chalk.blueBright },
-        TailwindCss: { icon: "🍃", color: chalk.green },
-        Hono: { icon: "🌐", color: chalk.blue },
-        ReactJs: { icon: "🍊", color: chalk.red }
+        NextJs: { icon: "🔵", color: chalk.cyan },
+        NodeJs: { icon: "🔵", color: chalk.cyan },
+        ExpressJs: { icon: "🔵", color: chalk.cyan },
+        Hono: { icon: "🔵", color: chalk.cyan },
+    },
+    databases: {
+        MongoDB: { icon: "🟪", color: chalk.magenta },
+        PostgreSQL: { icon: "🟪", color: chalk.magenta },
+        MySQL: { icon: "🟪", color: chalk.magenta },
+        Prisma: { icon: "🟪", color: chalk.magenta },
     },
     others: {
-        Docker: { icon: "🐳", color: chalk.blue },
-        mongodb: { icon: "🍃", color: chalk.green },
-        PostgreSQL: { icon: "🐘", color: chalk.blue },
-        Git: { icon: "🐙", color: chalk.blue },
-        Linux: { icon: "🐧", color: chalk.blue },
+        "Git/Github": { icon: "🔵", color: chalk.blue },
+        Docker: { icon: "🔵", color: chalk.blue },
+        AWS: { icon: "🔵", color: chalk.blue },
+        CloudFlare: { icon: "🔵", color: chalk.blue },
     },
 };
 
 const languagesArray = Object.entries(details.languages);
+const webArray = Object.entries(details.web);
+const librariesArray = Object.entries(details.libraries);
 const frameworksArray = Object.entries(details.frameworks);
+const databasesArray = Object.entries(details.databases);
 const othersArray = Object.entries(details.others);
 
 const languagesRows = [];
+const webRows = [];
+const librariesRows = [];
 const frameworksRows = [];
+const databasesRows = [];
 const othersRows = [];
+
 
 for (let i = 0; i < languagesArray.length; i += 4) {
     const slicedLanguages = languagesArray.slice(i, i + 4);
     const row = slicedLanguages
-        .map(([lang, { icon, color }]) => `${color(`${icon} ${lang}`)}`)
+        .map(([lang, { icon, color }]) => `${color(`   ${icon} ${lang}`)}`)
         .join(" ");
     languagesRows.push(row);
+}
+
+for (let i = 0; i < webArray.length; i += 4) {
+    const slicedWeb = webArray.slice(i, i + 4);
+    const row = slicedWeb
+        .map(([lang, { icon, color }]) => `${color(`   ${icon} ${lang}`)}`)
+        .join(" ");
+    webRows.push(row);
+}
+
+for (let i = 0; i < librariesArray.length; i += 4) {
+    const slicedLibraries = librariesArray.slice(i, i + 4);
+    const row = slicedLibraries
+        .map(([lang, { icon, color }]) => `${color(`   ${icon} ${lang}`)}`)
+        .join(" ");
+    librariesRows.push(row);
 }
 
 for (let i = 0; i < frameworksArray.length; i += 4) {
     const slicedFrameworks = frameworksArray.slice(i, i + 4);
     const row = slicedFrameworks
-        .map(([lang, { icon, color }]) => `${color(`${icon} ${lang}`)}`)
+        .map(([lang, { icon, color }]) => `${color(`   ${icon} ${lang}`)}`)
         .join(" ");
     frameworksRows.push(row);
+}
+
+for (let i = 0; i < databasesArray.length; i += 4) {
+    const slicedDatabases = databasesArray.slice(i, i + 4);
+    const row = slicedDatabases
+        .map(([lang, { icon, color }]) => `${color(`   ${icon} ${lang}`)}`)
+        .join(" ");
+    databasesRows.push(row);
 }
 
 for (let i = 0; i < othersArray.length; i += 4) {
     const slicedOthers = othersArray.slice(i, i + 4);
     const row = slicedOthers
-        .map(([lang, { icon, color }]) => `${color(`${icon} ${lang}`)}`)
+        .map(([lang, { icon, color }]) => `${color(`   ${icon} ${lang}`)}`)
         .join(" ");
     othersRows.push(row);
 }
 
 const info = [
-    `${chalk.bold("🦷 Name:")}${details.name}`,
-    `${chalk.bold("💡 About:")} ${details.occupation}`,
+    `${chalk.bold("⭐ Name:")}      ${details.name}`,
+    `${chalk.bold("💡 About:")}     ${details.occupation}`,
     `${chalk.bold("🌐 Website:")}   ${details.website}`,
-    `${chalk.bold("🐙 GitHub:")}    ${details.github}`,
-    `${chalk.bold("🐦 Twitter:")}   ${details.twitter}`,
-    `${chalk.bold("📧 Email:")} ${details.email}`,
+    `${chalk.bold("🔗 GitHub:")}    ${details.github}`,
+    `${chalk.bold("💬 Email:")}     ${details.email}`,
     `${chalk.bold("💻 Languages:")}`,
     ...languagesRows,
-    `${chalk.bold("🔧 Frameworks:")}`,
+    `${chalk.bold("🌐 Web:")}`,
+    ...webRows,
+    `${chalk.bold("🔧 Libraries:")}`,
+    ...librariesRows,
+    `${chalk.bold("🔨 Frameworks:")}`,
     ...frameworksRows,
-    `${chalk.bold("🔧 Others:")}`,
+    `${chalk.bold("💾 Databases:")}`,
+    ...databasesRows,
+    `${chalk.bold("🧰 Others:")}`,
     ...othersRows,
 ];
 
-const asciiLogoLines = asciiLogo.trim().split("\n");
+const asciiLogoLines = asciiLogo.split("\n");
 const infoLines = info;
 
 const logoWidth = Math.max(...asciiLogoLines.map((line) => line.length));
@@ -115,7 +166,7 @@ const output = outputLines.join("\n");
 const message = boxen(output, {
     padding: 1,
     margin: 1,
-    borderStyle: "classic",
+    borderStyle: "bold",
 });
 
 console.log(message);
